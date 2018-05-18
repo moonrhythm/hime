7ec400ef00f3a2b7635aeaeacf207f5af88aaace
acoshift <acoshift@gmail.com>
2018-05-18 21:30:35 +0700

    graceful: add graceful shutdown multiple apps

--- graceful.go
+++ graceful.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -99,4 +100,36 @@ func (gs *GracefulShutdownApp) ListenAndServe() error {
 // ListenAndServeTLS starts web server in graceful shutdown and tls mode
 func (gs *GracefulShutdownApp) ListenAndServeTLS(certFile, keyFile string) error {
 	return gs.start(func() error { return gs.App.listenAndServeTLS(certFile, keyFile) })
+}
+
+// GracefulShutdown runs multiple hime's app in graceful shutdown mode
+func GracefulShutdown(apps []*App) error {
+	wg := &sync.WaitGroup{}
+	errChan := make(chan error)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for _, app := range apps {
+		app := app
+		wg.Add(1)
+		go func() {
+			err := app.GracefulShutdown().ListenAndServe()
+			if err != http.ErrServerClosed {
+				errChan <- err
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		cancel()
+	}()
+
+	select {
+	case err := <-errChan:
+		return err
+	case <-ctx.Done():
+		return nil
+	}
 }
