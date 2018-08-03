da66e6daa5a7d70ba53b3c848a5c6b0792af764f
acoshift <acoshift@gmail.com>
2018-08-03 10:53:57 +0700

    test: global

--- global_test.go
+++ global_test.go
@@ -1,28 +1,27 @@
-package hime_test
+package hime
 
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/acoshift/hime"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGlobal(t *testing.T) {
 	t.Parallel()
 
-	app := hime.New()
+	app := New()
 
 	assert.Nil(t, app.Global("key1"))
 	assert.Nil(t, app.Global("key2"))
 
 	app.
-		Globals(hime.Globals{
+		Globals(Globals{
 			"key1": "value1",
 			"key2": "value2",
 		}).
-		Handler(hime.H(func(ctx *hime.Context) error {
+		Handler(H(func(ctx *Context) error {
 			assert.Equal(t, "value1", ctx.Global("key1"))
 			assert.Equal(t, "value2", ctx.Global("key2"))
 			assert.Nil(t, ctx.Global("invalid"))
@@ -36,4 +35,25 @@ func TestGlobal(t *testing.T) {
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
 	w := httptest.NewRecorder()
 	app.ServeHTTP(w, r)
+}
+
+func TestCloneGlobal(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Nil", func(t *testing.T) {
+		assert.Nil(t, cloneGlobals(nil))
+	})
+
+	t.Run("Normal", func(t *testing.T) {
+		g := Globals{
+			"a": 1,
+			"b": 2,
+		}
+
+		p := cloneGlobals(g)
+		p["a"] = 2
+		p["c"] = 3
+
+		assert.NotEqual(t, g, p)
+	})
 }
