e6c6dbcf9ce7370a10c97e31844820e69f504369
acoshift <acoshift@gmail.com>
2018-08-22 18:12:30 +0700

    add more app tests

--- app_test.go
+++ app_test.go
@@ -16,9 +16,8 @@ import (
 func TestApp(t *testing.T) {
 	t.Parallel()
 
-	app := New()
-
 	t.Run("TemplateFuncs", func(t *testing.T) {
+		app := New()
 		app.TemplateFunc("a", func() {})
 		assert.Len(t, app.templateFuncs, 1)
 
@@ -31,6 +30,7 @@ func TestApp(t *testing.T) {
 	})
 
 	t.Run("GracefulShutdown", func(t *testing.T) {
+		app := New()
 		assert.Nil(t, app.gs)
 		gs := app.GracefulShutdown()
 		assert.NotNil(t, app.gs)
@@ -49,8 +49,54 @@ func TestApp(t *testing.T) {
 	})
 
 	t.Run("Address", func(t *testing.T) {
+		app := New()
 		app.Address(":1234")
 		assert.Equal(t, ":1234", app.Addr)
+	})
+
+	t.Run("Clone", func(t *testing.T) {
+		app := New()
+		app.Routes(Routes{
+			"a": "1",
+			"b": "2",
+		})
+		app.Globals(Globals{
+			"q": "z",
+			"w": "x",
+		})
+		app.TLSConfig = Compatible()
+		app.GracefulShutdown()
+
+		app2 := app.Clone()
+		assert.NotNil(t, app2)
+
+		app2.Routes(Routes{
+			"a": "4",
+		}).Globals(Globals{
+			"q": "p",
+		})
+		app2.GracefulShutdown().Wait(10 * time.Second)
+
+		assert.NotEqual(t, app, app2)
+		assert.NotEqual(t, app.routes, app2.routes)
+		assert.NotEqual(t, app.globals, app2.globals)
+		assert.NotEqual(t, app.gs, app2.gs)
+		assert.NotNil(t, app2.TLSConfig)
+	})
+
+	t.Run("SelfSign empty param", func(t *testing.T) {
+		app := New()
+		app.SelfSign(SelfSign{})
+
+		assert.NotNil(t, app.TLSConfig)
+	})
+
+	t.Run("SelfSign invalid param", func(t *testing.T) {
+		app := New()
+		opt := SelfSign{}
+		opt.Key.Algo = "invalid"
+
+		assert.Panics(t, func() { app.SelfSign(opt) })
 	})
 }
 
@@ -74,31 +120,4 @@ func TestConfigServer(t *testing.T) {
 	app.configServer()
 	assert.NotEmpty(t, &app.srv)
 	assert.Equal(t, ":8080", app.srv.Addr)
-}
-
-func TestAppClone(t *testing.T) {
-	t.Parallel()
-
-	app := New().
-		Routes(Routes{
-			"a": "1",
-			"b": "2",
-		}).
-		Globals(Globals{
-			"q": "z",
-			"w": "x",
-		})
-
-	app2 := app.Clone()
-	assert.NotNil(t, app2)
-
-	app2.Routes(Routes{
-		"a": "4",
-	}).Globals(Globals{
-		"q": "p",
-	})
-
-	assert.NotEqual(t, app, app2)
-	assert.NotEqual(t, app.routes, app2.routes)
-	assert.NotEqual(t, app.globals, app2.globals)
 }
